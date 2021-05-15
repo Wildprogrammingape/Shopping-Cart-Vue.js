3faff28becff0c7c532039cffd3f417cf22170c1
Long <chenlong05188@gmail.com>
2021-05-14 23:19:08 -0400

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    // data: function(){} // shorthand,
+    data(){
+        return {
+            product: 'Socks',
+            discription: 'This is our product in store',
+            image: 'assets/images/socks_blue.jpg',
+            url: 'http://127.0.0.1:5500/index.html',
+            inStock: true,
+        }
+    }
+})
+
+
+
+
+
+
+
+// //匿名函数
+// const myfunc = function(){
+//     return 1;
+// }
+
+// //箭头函数
+// const myfunc = () => {
+//     return 1;
+// }
